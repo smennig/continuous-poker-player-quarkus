7184f1d545afb5237cf5eac899abd6178bfbe677
Simon Mennig <simon.mennig@doubleslash.de>
2023-02-23 19:40:25 +0100

    Raise for a Pair

--- src/main/java/org/continuouspoker/player/logic/Strategy.java
+++ src/main/java/org/continuouspoker/player/logic/Strategy.java
@@ -1,48 +1,63 @@
 package org.continuouspoker.player.logic;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.continuouspoker.player.model.Bet;
 import org.continuouspoker.player.model.Card;
+import org.continuouspoker.player.model.Rank;
 import org.continuouspoker.player.model.Table;
 
 public class Strategy {
 
-   public Bet decide(final Table table) {
-      System.out.println(table);
+    public Bet decide(final Table table) {
+        System.out.println(table);
 
-      var cards = getCards(table);
-      System.out.println(cards);
+        var cards = getCards(table);
+        System.out.println(cards);
 
-      return goAllIn(table);
-   }
+        if (isPair(getCommunityCards(table), getCards(table))){
+            return new Bet().bet(table.getMinimumBet() +40);
+        }
+        return  new Bet().bet(table.getMinimumBet());
+    }
 
-   private static Bet goAllIn(final Table table) {
-      return new Bet().bet(table.getPlayers().get(table.getActivePlayer()).getStack());
-   }
+    private static Bet goAllIn(final Table table) {
+        return new Bet().bet(table.getPlayers().get(table.getActivePlayer()).getStack());
+    }
 
-   private  List<Card> getCards(final Table table) {
-      return table.getPlayers()
-              .stream()
-              .filter(player1 -> player1.getName().equals("die-planlosen-bluffer"))
-              .map(player1 -> player1.getCards())
-              .findFirst()
-              .get();
-   }
-   private  List<Card> getCommunityCards(final Table table) {
-      return table.getCommunityCards();
-   }
+    private List<Card> getCards(final Table table) {
+        return table.getPlayers()
+                .stream()
+                .filter(player1 -> player1.getName().equals("die-planlosen-bluffer"))
+                .map(player1 -> player1.getCards())
+                .findFirst()
+                .get();
+    }
 
-   private boolean isPair (List<Card> communityCards,List<Card> playerCards  ){
-      communityCards.addAll(playerCards);
+    private List<Card> getCommunityCards(final Table table) {
+        return table.getCommunityCards();
+    }
 
-      for(Card card : communityCards){
-
-      }
-      return false;
-   }
-/*   private int countPairs (List<Card> communityCards,List<Card> playerCards  ){
-      communityCards.addAll(playerCards)
-   }*/
+    private boolean isPair(List<Card> communityCards, List<Card> playerCards) {
+        communityCards.addAll(playerCards);
+        Map<Rank, Integer> pairs = new HashMap<>();
+        for (Card card : communityCards) {
+            final Integer pair = pairs.get(card.getRank());
+            if (pair == null) {
+                pairs.put(card.getRank(), 1);
+            } else {
+                pairs.put(card.getRank(), pair + 1);
+            }
+        }
+        for (Rank rank : pairs.keySet()) {
+            if (pairs.get(rank) != null && pairs.get(rank) >= 2) {
+                return true;
+            }
+        }
+        return false;
+    }
 
 }
