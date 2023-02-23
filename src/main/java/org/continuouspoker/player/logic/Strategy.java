f80a4a64e77e30592adf05a39fce73db6ff93e12
Simon Mennig <simon.mennig@doubleslash.de>
2023-02-23 19:19:52 +0100

    Add get Cards method

--- src/main/java/org/continuouspoker/player/logic/Strategy.java
+++ src/main/java/org/continuouspoker/player/logic/Strategy.java
@@ -1,13 +1,29 @@
 package org.continuouspoker.player.logic;
 
+import java.util.List;
+
 import org.continuouspoker.player.model.Bet;
+import org.continuouspoker.player.model.Card;
 import org.continuouspoker.player.model.Table;
 
 public class Strategy {
 
    public Bet decide(final Table table) {
       System.out.println(table);
-      return new Bet().bet(0);
+
+      var cards = getCards(table);
+      System.out.println(cards);
+
+      return new Bet().bet(table.getMinimumBet());
+   }
+
+   private  List<Card> getCards(final Table table) {
+      return table.getPlayers()
+              .stream()
+              .filter(player1 -> player1.getName().equals("die-planlosen-bluffer"))
+              .map(player1 -> player1.getCards())
+              .findFirst()
+              .get();
    }
 
 }
